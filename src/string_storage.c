eafdaf9237be76aa8c5b50caf6ae7a5316995555
David Endrych <david.endrych@gmail.com>
2017-09-30 14:42:00 +0200

    Implement String storage
    There is actual some errors
    
    Issue: #8

--- src/string_storage.c
+++ src/string_storage.c
@@ -1,24 +1,46 @@
 #include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
 
 #define ALLOC_SPACE 100
 
 static char* _string_storage = NULL;
 
-char* add_string_to_storage(char* array,int length)
+char* add_string_to_storage(char* array)
 {
-    static int pos;
-    static int space;
-    if(_string_storage == NULL)
+    if(array != NULL)
     {
-        _string_storage = (char*)malloc(ALLOC_SPACE * sizeof(char));
-        pos = 0;
-        space = ALLOC_SPACE;
-    }
-    else if(pos + length > space)
-    {
-        _string_storage = realloc(_string_storage,pos + length + ALLOC_SPACE);
-    }
+        int length = strlen(array);
+        static int pos;
+        static int space;
+        if(_string_storage == NULL)
+        {
+            _string_storage = (char*)malloc((ALLOC_SPACE + length) * sizeof(char));
+            if(_string_storage == NULL)
+            {
+                fprintf(stderr,"Problem with memory\n");
+                return NULL;
+            }
+            pos = 0;
+            space = ALLOC_SPACE + length;
+        }
+        else if(pos + length > space)
+        {
+            _string_storage = realloc(_string_storage,pos + strlen(array) + ALLOC_SPACE);
+            if(_string_storage == NULL)
+            {
+                fprintf(stderr,"Problem with memory\n");
+                return NULL;
+            }
+        }
+        char* result = NULL;
+        result = strcpy(&_string_storage[pos],array);
+        result[length] = '#';
+        pos = length + 1;
 
+        return result;
+    }
+    return NULL;
 }
 
 void destruct_storage()
--- tests/string_storage_test.c
+++ tests/string_storage_test.c
@@ -1,9 +1,55 @@
 #include <stdio.h>
 #include "../src/string_storage.h"
+#include <stdlib.h>
 
 int main()
 {
-    add_string_to_storage("ahoj",4);
+    char* ahoj_string = add_string_to_storage("ahoj");
+    int pos=0;
+    while(ahoj_string[pos]!='#')
+    {
+        putchar(ahoj_string[pos]);
+        pos++;
+    }
+    putchar('\n');
+    char * lengt_str = NULL;
+    lengt_str = (char*) malloc(180 * sizeof(char));
+    char c = 'a';
+    for(int i=0;i<178;i++)
+    {
+        lengt_str[i] = c;
+        if(c != 'z')
+            c++;
+        else
+            c = 'a';
+    }
+    lengt_str[179] = '\n';
+    char* length_string = add_string_to_storage(lengt_str);
+    pos=0;
+    while(length_string[pos]!='#')
+    {
+        putchar(length_string[pos]);
+        pos++;
+    }
+    putchar('\n');
+
+    pos=0;
+    while(ahoj_string[pos]!='#')
+    {
+        putchar(ahoj_string[pos]);
+        pos++;
+    }
+    putchar('\n');
+
+    pos=0;
+    while(length_string[pos]!='#')
+    {
+        putchar(length_string[pos]);
+        pos++;
+    }
+    putchar('\n');
+
+    free(lengt_str);
     destruct_storage();
     return 0;
 }
