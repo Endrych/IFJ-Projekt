20a89cea206c0a1c89640712d784f2df3610e606
petrzubalik <xzubal04@stud.fit.vutbr.cz>
2017-11-15 19:58:28 +0100

    Uprava struktury pro rozliseni symbolu funkce a promenne

--- src/symtable.c
+++ src/symtable.c
@@ -1,11 +1,12 @@
 #include "symtable.h"
 #include "string_storage.h"
+#include "set_values.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
-Tsymtab_item *symtab_search(Tsymtab *sym_table, Token *token)
+Tsymtab_item *symtab_search(Tsymtab *sym_table, Token *token, Telement_type type)
 {
 	int position = token->atribute.int_value;
 	char *key = get_string(position);
@@ -14,10 +15,15 @@ Tsymtab_item *symtab_search(Tsymtab *sym_table, Token *token)
 
 	while(temp)
 	{	
-		temp->declared = false;
 		if ((strcmp(temp->key, key)) == 0)
-		{
-			temp->declared = true;
+		{	
+			if (type == type_variable)
+			{	
+				temp->type_strct.variable->declared = true;
+			} else if (type == type_function)
+			{
+				temp->type_strct.function->declared = true;
+			}
 			free(key);
 			return temp;
 		}
@@ -73,6 +79,13 @@ void symtab_free(Tsymtab *sym_table)
 		{
 			Tsymtab_item *current_item = temp_item;
 			temp_item = temp_item->next;
+			if (current_item->type == type_variable)
+			{
+				free(current_item->type_strct.variable);
+			} else if (current_item->type == type_function)
+			{
+				free(current_item->type_strct.function);
+			}
 			free(current_item->key);
 			free(current_item);
 		}
@@ -83,25 +96,45 @@ void symtab_free(Tsymtab *sym_table)
 
 
 
-Tsymtab_item *symtab_insert(Tsymtab *sym_table, Token *token)
+Tsymtab_item *symtab_insert(Tsymtab *sym_table, Token *token, Telement_type type)
 {	
+
 	int position = token->atribute.int_value;
 	char *key = get_string(position);
 	unsigned int index = hash_func(key) % sym_table->size;
-	Tsymtab_item *temp = symtab_search(sym_table, token);
+	Tsymtab_item *temp = symtab_search(sym_table, token, type);
 
 	if (temp == NULL)
 	{
 		// allocate memory for new item
-		Tsymtab_item *new_item = (Tsymtab_item *) malloc(sizeof(*new_item));
+		Tsymtab_item *new_item = (Tsymtab_item *) malloc(sizeof(Tsymtab_item));
 		if (new_item == NULL)
 		{	
 			fprintf(stderr, "Memory allocation failed\n");
 			return NULL;
 		}
 
+		if (type == type_variable)
+		{
+			new_item->type_strct.variable = (struct var_item *) malloc(sizeof(struct var_item));
+			if (new_item->type_strct.variable == NULL)
+			{
+				fprintf(stderr, "Memory allocation failed\n");
+				return NULL;
+			}
+		} else if (type == type_function)
+		{
+			new_item->type_strct.function = (struct fc_item *) malloc(sizeof(struct fc_item ));
+			if (new_item->type_strct.function == NULL)
+			{
+				fprintf(stderr, "Memory allocation failed\n");
+				return NULL;
+			}
+		}
+
+
 		new_item->key = key;
-		new_item->token = token;
+		new_item->type = type;
 
 		// empty list
 		if (sym_table->symtab_list[index] == NULL)
@@ -177,6 +210,8 @@ unsigned int hash_func( char *key)
 /*
 int main()
 {	
+	Telement_type type = type_variable;
+	Telement_type type2 = type_function;
 	Token *token_a;
 	token_a = malloc(sizeof(Token));
 	Token *token_b;
@@ -198,36 +233,63 @@ int main()
 	int a = add_string_to_storage("aaoj");
 	token_a->type = type_string;
 	(token_a->atribute).int_value = a; 
+
 	int b = add_string_to_storage("aaoj");
 	token_b->type = type_string;
 	token_b->atribute.int_value = b;
+
 	int c = add_string_to_storage("aaoj");
 	token_c->type = type_string;
 	token_c->atribute.int_value = c;
+
 	int d = add_string_to_storage("adoj");
 	token_d->type = type_string;
 	token_d->atribute.int_value = d;
+
 	int n = add_string_to_storage("anoj");
 	token_n->type = type_string;
 	token_n->atribute.int_value = n;
+
 	int o = add_string_to_storage("aooj");
 	token_o->type = type_string;
 	token_o->atribute.int_value = o;
+
 	int p = add_string_to_storage("zdar");
 	token_p->type = type_string;
 	token_p->atribute.int_value = p;
+
 	int q = add_string_to_storage("aqoj");
 	token_q->type = type_string;
 	token_q->atribute.int_value = q;
-	symtab_insert(tab, token_a);
-	symtab_insert(tab, token_b);
-	symtab_insert(tab, token_c);
-	symtab_insert(tab, token_d);
-	symtab_insert(tab, token_n);
-	symtab_insert(tab, token_o);
-	symtab_insert(tab, token_p);
-	symtab_insert(tab, token_q);
 
+	symtab_insert(tab, token_a, type);
+	symtab_insert(tab, token_b, type);
+	symtab_insert(tab, token_c, type);
+	symtab_insert(tab, token_d, type);
+	symtab_insert(tab, token_n, type2);
+	symtab_insert(tab, token_o, type2);
+	symtab_insert(tab, token_p, type2);
+	symtab_insert(tab, token_q, type2);
+
+	Tvalue value;
+	value.value_int = 10;
+	Tvariable_type type3;
+	type3 = type_int;
+	Tsymtab_item *temp = tab->symtab_list[1];
+
+	set_type_variable(temp->type_strct.variable, value, type3);
+	printf("%s  %d  %d\n", temp->key, temp->type_strct.variable->value.value_int, temp->type_strct.variable->type);
+
+	Tsymtab_item *temp2 = tab->symtab_list[3];
+	init_type_function(temp2->type_strct.function);
+	set_type_function(temp2->type_strct.function, type3);
+	set_args_function(temp2->type_strct.function, "cus", type_int, value);
+	set_args_function(temp2->type_strct.function, "cau", type_int, value);
+	printf("%s->%s(%d)->%s(%d)\n", temp2->key, temp2->type_strct.function->arguments[0].key,\
+								 temp2->type_strct.function->arguments[0].value.value_int, \
+								 temp2->type_strct.function->arguments->key, \
+								 temp2->type_strct.function->arguments->value.value_int);
+	free_args_function(temp2->type_strct.function);
 	for (unsigned int i = 0; i < tab->size; i++)
 	{
 		Tsymtab_item *temp = tab->symtab_list[i];
@@ -255,6 +317,5 @@ int main()
 	free(token_p);
 	free(token_q);
 	return 0;
-}
+}*/
 
-*/
