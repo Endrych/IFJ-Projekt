6c39c8b4a2255ffba03bf88dc598b17913435bc7
David Endrych <david.endrych@gmail.com>
2017-11-21 15:39:34 +0100

    Make prom variable to test

--- tests/gen_test.c
+++ tests/gen_test.c
@@ -8,12 +8,26 @@
 #include "../src/generator.h"
 
 extern Tsymtab * symtab;
+#define N 0
 
 int main(){
+    printf("prom je definovana na 5\n");
     symtab = symtab_init(17);
-    while(1){
-        printf("Zadejte vyraz\n");
-        PrecendentOutput * out = precedence_analysis(NULL);
-        generate_expression(out->Tree);
+    Token x;
+    x.type = type_id;
+    char * x_id = "prom";
+    x.atribute.int_value = add_string_to_storage(x_id);
+    Tsymtab_item * item =  symtab_insert(symtab,&x,type_variable);
+    item->type = type_int;
+    item->type_strct.variable->declared = true;
+    item->type_strct.variable->type = type_int;
+    item->type_strct.variable->value.value_int = 5;
+    switch(N){
+        case 0:
+            printf("Zadejte vyraz\n");
+            PrecendentOutput * out = precedence_analysis(NULL);
+            generate_expression(out->Tree);
+            break;
     }
+   
 }
