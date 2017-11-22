8a5c4108b21aa76f9cee864e48131f5add5f0fe0
petrzubalik <xzubal04@stud.fit.vutbr.cz>
2017-11-22 21:38:24 +0100

    funkce pro vlozeni built-in funkci do tabulky symbolu

---
+++ src/built_in.h
@@ -0,0 +1,15 @@
+#ifndef BUILT_IN_H
+#define BUILT_IN_H
+
+void insert_length();
+
+void insert_substr();
+
+void insert_asc();
+
+void insert_chr();
+
+void insert_built_in();
+
+
+#endif /* BUILT_IN_H */
