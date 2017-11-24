b8076cd4691c984f62019e9cc49f946c1d0634e4
David Endrych <david.endrych@gmail.com>
2017-11-24 16:54:50 +0100

    Release

---
+++ release/built_in.h
@@ -0,0 +1,23 @@
+/* Nazev - Implementace prekladace imperativniho jazyka IFJ17
+ * Petr Zubalik - xzubal04
+ * Marek Kukucka - xkukuc04
+ * Jan Koci - xkocij01
+ * David Endrych - xendry02
+ */
+
+
+#ifndef BUILT_IN_H
+#define BUILT_IN_H
+
+void insert_length();
+
+void insert_substr();
+
+void insert_asc();
+
+void insert_chr();
+
+void insert_built_in();
+
+
+#endif /* BUILT_IN_H */
