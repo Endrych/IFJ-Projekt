597b9494936ad3808cf46a91568060dcc87fafaa
kukucka <kukin1996@seznam.cz>
2017-11-29 13:47:40 +0100

    Tester extension

--- tests/automated-tester/tester.sh
+++ tests/automated-tester/tester.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 ARGUMENT=$1
+if [ $# -eq 2 ]; then
+    FOLDER_NUMBER=$2
+fi
 NUMBER_OF_FOLDERS=$(ls ./tests| wc -l)
 TOITERATE=1
 FOLDER_TOITERATE=1
@@ -22,6 +25,36 @@ restoreBack () {
 }
 if [ "$ARGUMENT" = "-d" ]; then
     restoreBack
+elif [ $# -eq 2 ];then
+    if [ ! -f $ARGUMENT ]; then
+        printf "Vami zadany argument \"$ARGUMENT\" neni pozice souboru\n"
+    else
+        restoreBack
+        FOLDER_NAME=$(ls ./tests | grep $FOLDER_NUMBER-)
+        printf "____________\n" >> results.txt
+        printf "$FOLDER_NAME:\n" >> results.txt
+        printf "¯¯¯¯¯¯¯¯¯¯¯¯\n" >> results.txt
+        
+        NUMBER=$(ls ./tests/$FOLDER_NAME| wc -l)
+        ITERATE=$(( $NUMBER / 2 ))
+        while [ $TOITERATE -le $ITERATE ]
+        do
+            ./$ARGUMENT < ./tests/$FOLDER_NAME/$TOITERATE.code > ./tests/$FOLDER_NAME/$TOITERATE.ifj 
+            COMPILER_RETURN_CODE=$?
+            ./ic17int ./tests/$FOLDER_NAME/$TOITERATE.ifj  > ./tests/$FOLDER_NAME/$TOITERATE.out
+            INTERPRET_RETURN_CODE=$?
+            if diff ./tests/$FOLDER_NAME/$TOITERATE.out ./tests/$FOLDER_NAME/$TOITERATE.correct;  then
+                printf "$TOITERATE\342\234\224 - Return codes- C: $COMPILER_RETURN_CODE I: $INTERPRET_RETURN_CODE\n" >> results.txt
+
+                rm ./tests/$FOLDER_NAME/$TOITERATE.ifj
+                rm ./tests/$FOLDER_NAME/$TOITERATE.out
+            else
+                printf "$TOITERATE\342\234\227 - Return codes- C: $COMPILER_RETURN_CODE I: $INTERPRET_RETURN_CODE\n" >> results.txt
+                diff ./tests/$FOLDER_NAME/$TOITERATE.out ./tests/$FOLDER_NAME/$TOITERATE.correct  > ./tests/$FOLDER_NAME/$TOITERATE.dif 
+            fi
+            TOITERATE=$(($TOITERATE + 1))
+        done
+    fi
 elif [ "$ARGUMENT" = "-h" ]; then
     cat ./help_print
 elif [ "$#" -eq 1 ]; then
@@ -33,7 +66,8 @@ elif [ "$#" -eq 1 ]; then
         do
             FOLDER_NAME=$(ls ./tests | grep $FOLDER_TOITERATE-)
             printf "____________\n" >> results.txt
-            printf "Folder $FOLDER_NAME:\n" >> results.txt
+            printf "$FOLDER_NAME:\n" >> results.txt
+            printf "¯¯¯¯¯¯¯¯¯¯¯¯\n" >> results.txt
             NUMBER=$(ls ./tests/$FOLDER_NAME| wc -l)
             ITERATE=$(( $NUMBER / 2 ))
             while [ $TOITERATE -le $ITERATE ]
