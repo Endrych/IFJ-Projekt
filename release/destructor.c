dff70f28e6a7dd07319dcc10b99f3b4c7ef968b6
David Endrych <david.endrych@gmail.com>
2017-12-01 09:37:46 +0100

    Release

---
+++ release/destructor.c
@@ -0,0 +1,48 @@
+#include "destructor.h"
+#include "string_storage.h"
+#include "stack.h"
+#include "token.h"
+#include "at_que.h"
+#include "frame.h"
+/* Nazev - Implementace prekladace imperativniho jazyka IFJ17
+ * Petr Zubalik - xzubal04
+ * Marek Kukucka - xkukuc04
+ * Jan Koci - xkocij01
+ * David Endrych - xendry02
+ */
+
+#include "gen_stacks.h"
+#include "error.h"
+#include <stdlib.h>
+
+extern Tsymtab *symtab;
+extern char* _string_storage;
+extern TSItem * _token_storage;
+
+void dispose_all(tStack *stack, ATLeaf *leaf, ATQueue* s, eQueue* s2, TFstack *frame_stack, GPStack *s3)
+{
+	destruct_storage();
+	destruct_token_storage();
+	dispose_at(leaf);
+	stackDestruct(stack);
+	symtab_free(symtab);
+	queDestruct(s);
+	equeDestruct(s2);
+	FS_destruct(frame_stack);
+	gsptr_stackDestruct(s3);
+
+}
+
+void dispose(){
+	destruct_storage();
+
+	destruct_token_storage();
+
+	symtab_free(symtab);
+}
+
+void dispose_global()
+{
+	dispose();
+	exit(COMPILER_ERROR);
+}
---
+++ release/lexical.h
@@ -0,0 +1,15 @@
+/* Nazev - Implementace prekladace imperativniho jazyka IFJ17
+ * Petr Zubalik - xzubal04
+ * Marek Kukucka - xkukuc04
+ * Jan Koci - xkocij01
+ * David Endrych - xendry02
+ */
+
+#include "token.h"
+
+#ifndef LEXICAL_H
+#define LEXICAL_H
+
+Token* get_token();
+
+#endif
